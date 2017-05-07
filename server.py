035580804f6b101ef9382fca0ea26fb4b6ef2db7
Vidar Minkovsky <vmsailor@gmail.com>
2017-05-07 18:13:42 -0400

    clean up

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-# import os
-# import sys
 import errno
 import select
 import socket
@@ -9,20 +7,6 @@ BACKLOG = 5
 
 games = [None] * 100
 users = {}
-
-
-def drawBoard(board):
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
 
 
 def makeMove(board, letter, move):
@@ -71,19 +55,16 @@ def move(gameid, move):
     game = games[gameid]
     if game['isPlaying']:
         if game['turn'] == 'X':
-            # drawBoard(game['board'])
             move = checkValidMove(game['board'], move)
             if move == -1:
                 return -1
             makeMove(game['board'], 'X', move)
             if isWinner(game['board'], 'X'):
-                # drawBoard(game['board'])
                 print('X won the game')
                 game['isPlaying'] = False
                 return 1
             else:
                 if isBoardFull(game['board']):
-                    # drawBoard(game['board'])
                     print('Tie')
                     return 3
                 else:
@@ -92,19 +73,16 @@ def move(gameid, move):
                     return 0
 
         else:
-            # drawBoard(game['board'])
             move = checkValidMove(game['board'], move)
             if move == -1:
                 return -1
             makeMove(game['board'], 'O', move)
             if isWinner(game['board'], 'O'):
-                # drawBoard(game['board'])
                 print('O won the game')
                 game['isPlaying'] = False
                 return 2
             else:
                 if isBoardFull(game['board']):
-                    # drawBoard(game['board'])
                     print('Tie')
                     return 3
                 else:
@@ -113,31 +91,21 @@ def move(gameid, move):
                     return 0
     else:
         print("Tried to move in a game that finished")
-        # return -1 for invalid, 0 for good in progress, 1 for win, 2 for win
 
 
 def serve_forever(host, port):
-    # create, bind. listen
     lstsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # re-use the port
     lstsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # put listening socket into non-blocking mode
     lstsock.setblocking(0)
-
     lstsock.bind((host, port))
     lstsock.listen(BACKLOG)
-
-    print('Listening on port %d ...' % port)
-
-    # read, write, exception lists with sockets to poll
+    print('Serving up some Tic-Tac-Toe on ' + str(host) + ':' + str(port) + '...')
     rlist, wlist, elist = [lstsock], [], []
 
     while True:
-        # block in select
         readables, writables, exceptions = select.select(rlist, wlist, elist)
-        # print(readables)
         for sock in readables:
-            if sock is lstsock:  # new client connection, we can accept now
+            if sock is lstsock:
                 try:
                     conn, client_address = lstsock.accept()
                 except IOError as e:
@@ -146,25 +114,13 @@ def serve_forever(host, port):
                         continue
                     else:
                         raise
-                # add the new connection to the 'read' list to poll
-                # in the next loop cycle
                 rlist.append(conn)
             else:
-                # read a line that tells us how many bytes to write
                 bytes = sock.recv(1024).decode()
-                # print(bytes)
-                if not bytes:  # connection closed by client
+                if not bytes:
                     sock.close()
                     rlist.remove(sock)
                 else:
-                    # print(sock)
-                    # print ('Got request to send %s bytes. '
-                    #       'Sending them all to...' % bytes)
-                    # send them all
-                    # XXX: this is cheating, we should use 'select' and wlist
-                    # to determine whether socket is ready to be written to
-                    # data = os.urandom(int(bytes))
-                    # sock.sendall(data)
                     info = bytes.split('#')
                     print(info)
                     if info[0] == '100':
@@ -221,12 +177,10 @@ def serve_forever(host, port):
                                 print('out of gid')
                                 sock.sendall(('408#' + info[1] + '#Out of game slots').encode())
                     elif info[0] == '204':
-                        # inform players
-                        # close game
-                        # close connections
                         game = games[int(info[2])]
-                        users[games['X']] = 'availible'
-                        users[games['O']] = 'availible'
+                        users[game['X']] = 'availible'
+                        users[game['O']] = 'availible'
+                        del users[info[1]]
                         game['isPlaying'] = False
                         # game = None
                         sock.sendall(("205#" + info[1] + '#' + info[2]).encode())
@@ -240,24 +194,24 @@ def serve_forever(host, port):
                         elif ret == 1:
                             print('214#' + info[2] + '#' + 'X Won!')
                             game = games[int(info[2])]
-                            users[games['X']] = 'availible'
-                            users[games['O']] = 'availible'
+                            users[game['X']] = 'availible'
+                            users[game['O']] = 'availible'
                             game['isPlaying'] = False
                             # game = None
                             sock.sendall(('214#' + info[2] + '#' + 'X Won!').encode())
                         elif ret == 2:
                             print('2')
                             game = games[int(info[2])]
-                            users[games['X']] = 'availible'
-                            users[games['O']] = 'availible'
+                            users[game['X']] = 'availible'
+                            users[game['O']] = 'availible'
                             game['isPlaying'] = False
                             # game = None
                             sock.sendall(('214#' + info[2] + '#' + 'O Won!').encode())
                         elif ret == 3:
                             print('3')
                             game = games[int(info[2])]
-                            users[games['X']] = 'availible'
-                            users[games['O']] = 'availible'
+                            users[game['X']] = 'availible'
+                            users[game['O']] = 'availible'
                             game['isPlaying'] = False
                             # game = None
                             sock.sendall(('214#' + info[2] + '#' + 'Tie!').encode())
@@ -273,16 +227,13 @@ def serve_forever(host, port):
 def main():
     parser = optparse.OptionParser()
     parser.add_option(
-        '-i', '--host', dest='host', default='0.0.0.0',
-        help='Hostname or IP address. Default is 0.0.0.0'
+        '-i', '--host', dest='host', default='localhost',
+        help='Hostname or IP address. Default is localhost'
     )
-
     parser.add_option(
-        '-p', '--port', dest='port', type='int', default=2000,
-        help='Port. Default is 2000')
-
+        '-p', '--port', dest='port', type='int', default=69,
+        help='Port. Default is 69')
     options, args = parser.parse_args()
-
     serve_forever(options.host, options.port)
 
 
