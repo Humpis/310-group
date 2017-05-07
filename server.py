6690e91a6d59f1985104669c287945dc3e3d5627
Vidar Minkovsky <vmsailor@gmail.com>
2017-05-07 14:22:42 -0400

    formatting

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import os
-import sys
+# import os
+# import sys
 import errno
 import select
 import socket
@@ -7,8 +7,9 @@ import optparse
 
 BACKLOG = 5
 
-games = [{},{},{},{},{}]
+games = [{}, {}, {}, {}, {}]
 users = []
+
 
 def drawBoard(board):
     print('   |   |')
@@ -23,32 +24,38 @@ def drawBoard(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
+
 def makeMove(board, letter, move):
     board[move] = letter
 
+
 def isWinner(bo, le):
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or
-    (bo[4] == le and bo[5] == le and bo[6] == le) or
-    (bo[1] == le and bo[2] == le and bo[3] == le) or
-    (bo[7] == le and bo[4] == le and bo[1] == le) or
-    (bo[8] == le and bo[5] == le and bo[2] == le) or
-    (bo[9] == le and bo[6] == le and bo[3] == le) or
-    (bo[7] == le and bo[5] == le and bo[3] == le) or
-    (bo[9] == le and bo[5] == le and bo[1] == le))
+            (bo[4] == le and bo[5] == le and bo[6] == le) or
+            (bo[1] == le and bo[2] == le and bo[3] == le) or
+            (bo[7] == le and bo[4] == le and bo[1] == le) or
+            (bo[8] == le and bo[5] == le and bo[2] == le) or
+            (bo[9] == le and bo[6] == le and bo[3] == le) or
+            (bo[7] == le and bo[5] == le and bo[3] == le) or
+            (bo[9] == le and bo[5] == le and bo[1] == le))
+
 
 def isSpaceFree(board, move):
     return board[move] == ' '
+
 
 def checkValidMove(board, move):
     if move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
         move = -1
     return int(move)
 
+
 def isBoardFull(board):
     for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
     return True
+
 
 def newgame(gameid, X, O):
     games[gameid] = {}
@@ -59,23 +66,24 @@ def newgame(gameid, X, O):
     games[gameid]['X'] = X
     games[gameid]['O'] = O
 
+
 def move(gameid, move):
     game = games[gameid]
     if game['isPlaying']:
         if game['turn'] == 'X':
-            #drawBoard(game['board'])
+            # drawBoard(game['board'])
             move = checkValidMove(game['board'], move)
             if move == -1:
                 return -1
             makeMove(game['board'], 'X', move)
             if isWinner(game['board'], 'X'):
-                #drawBoard(game['board'])
+                # drawBoard(game['board'])
                 print('X won the game')
                 game['isPlaying'] = False
                 return 1
             else:
                 if isBoardFull(game['board']):
-                    #drawBoard(game['board'])
+                    # drawBoard(game['board'])
                     print('Tie')
                     return 3
                 else:
@@ -84,19 +92,19 @@ def move(gameid, move):
                     return 0
 
         else:
-            #drawBoard(game['board'])
+            # drawBoard(game['board'])
             move = checkValidMove(game['board'], move)
             if move == -1:
                 return -1
             makeMove(game['board'], 'O', move)
             if isWinner(game['board'], 'O'):
-                #drawBoard(game['board'])
+                # drawBoard(game['board'])
                 print('O won the game')
                 game['isPlaying'] = False
                 return 2
             else:
                 if isBoardFull(game['board']):
-                    #drawBoard(game['board'])
+                    # drawBoard(game['board'])
                     print('Tie')
                     return 3
                 else:
@@ -106,6 +114,7 @@ def move(gameid, move):
     else:
         print("Tried to move in a game that finished")
         # return -1 for invalid, 0 for good in progress, 1 for win, 2 for win
+
 
 def serve_forever(host, port):
     # create, bind. listen
@@ -118,7 +127,7 @@ def serve_forever(host, port):
     lstsock.bind((host, port))
     lstsock.listen(BACKLOG)
 
-    print ('Listening on port %d ...' % port)
+    print('Listening on port %d ...' % port)
 
     # read, write, exception lists with sockets to poll
     rlist, wlist, elist = [lstsock], [], []
@@ -128,7 +137,7 @@ def serve_forever(host, port):
         readables, writables, exceptions = select.select(rlist, wlist, elist)
 
         for sock in readables:
-            if sock is lstsock: # new client connection, we can accept now
+            if sock is lstsock:  # new client connection, we can accept now
                 try:
                     conn, client_address = lstsock.accept()
                 except IOError as e:
@@ -144,12 +153,12 @@ def serve_forever(host, port):
                 # read a line that tells us how many bytes to write
                 bytes = sock.recv(1024).decode()
                 # print(bytes)
-                if not bytes: # connection closed by client
+                if not bytes:  # connection closed by client
                     sock.close()
                     rlist.remove(sock)
                 else:
                     # print(sock)
-                   # print ('Got request to send %s bytes. '
+                    # print ('Got request to send %s bytes. '
                     #       'Sending them all to...' % bytes)
                     # send them all
                     # XXX: this is cheating, we should use 'select' and wlist
@@ -167,30 +176,41 @@ def serve_forever(host, port):
                             sock.sendall(('401#').encode())
                     elif info[0] == '200':
                         newgame(int(info[2]), info[1], info[1])
-                        sock.sendall(('201#' + info[1] + '#' + info[2]).encode())
-                        #print(('213#' + info[2] + '#' + ''.join(games[int(gameid)]['board']) + '#' + info[1]))
-                        #sock.sendall(('213#' + info[2] + '#' + ''.join(games[int(gameid)]['board']) + '#' + info[1] + '#' + info[1] + '#' + info[1]).encode())
-                        print('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O'])
-                        sock.sendall(('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O']).encode())
+                        sock.sendall(
+                            ('201#' + info[1] + '#' + info[2]).encode())
+                        # print(('213#' + info[2] + '#' + ''.join(games[int(gameid)]['board']) + '#' + info[1]))
+                        # sock.sendall(('213#' + info[2] + '#' +
+                        # ''.join(games[int(gameid)]['board']) + '#' + info[1]
+                        # + '#' + info[1] + '#' + info[1]).encode())
+                        print('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(
+                            info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O'])
+                        sock.sendall(('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(
+                            info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O']).encode())
 
-                       # if eerror send 408
+                        # if eerror send 408
                     elif info[0] == '210':
                         ret = move(int(info[2]), info[3])
                         if ret == 0 or ret == -1:
                             print('0 or -1')
-                            sock.sendall(("211#" + info[1] + '#' + info[3]).encode())
+                            sock.sendall(
+                                ("211#" + info[1] + '#' + info[3]).encode())
                         elif ret == 1:
                             print('214#' + info[2] + '#' + 'X Won!')
-                            sock.sendall(('214#' + info[2] + '#' + 'X Won!').encode())
+                            sock.sendall(
+                                ('214#' + info[2] + '#' + 'X Won!').encode())
                         elif ret == 2:
                             print('2')
-                            sock.sendall(('214#' + info[2] + '#' + 'O Won!').encode())
+                            sock.sendall(
+                                ('214#' + info[2] + '#' + 'O Won!').encode())
                         elif ret == 3:
                             print('3')
-                            sock.sendall(('214#' + info[2] + '#' + 'Tie!').encode())
+                            sock.sendall(
+                                ('214#' + info[2] + '#' + 'Tie!').encode())
                     elif info[0] == '212':
-                        print('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O'])
-                        sock.sendall(('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O']).encode())
+                        print('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(
+                            info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O'])
+                        sock.sendall(('213#' + info[2] + '#' + ''.join(games[int(info[2])]['board']) + '#' + games[int(
+                            info[2])]['turnuid'] + '#' + games[int(info[2])]['X'] + '#' + games[int(info[2])]['O']).encode())
 
                     else:
                         sock.sendall(("Not supported rn").encode())
@@ -201,7 +221,7 @@ def main():
     parser.add_option(
         '-i', '--host', dest='host', default='0.0.0.0',
         help='Hostname or IP address. Default is 0.0.0.0'
-        )
+    )
 
     parser.add_option(
         '-p', '--port', dest='port', type='int', default=2000,
@@ -210,6 +230,7 @@ def main():
     options, args = parser.parse_args()
 
     serve_forever(options.host, options.port)
+
 
 if __name__ == '__main__':
     main()
