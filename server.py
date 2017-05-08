0b37d66d4416653365be7cafe92570893bcc49d8
Vidar Minkovsky <vmsailor@gmail.com>
2017-05-07 20:40:52 -0400

    shes done

--- server.py
+++ server.py
@@ -60,12 +60,10 @@ def move(gameid, move):
                 return -1
             makeMove(game['board'], 'X', move)
             if isWinner(game['board'], 'X'):
-                print('X won the game')
                 game['isPlaying'] = False
                 return 1
             else:
                 if isBoardFull(game['board']):
-                    print('Tie')
                     return 3
                 else:
                     game['turn'] = 'O'
@@ -78,22 +76,18 @@ def move(gameid, move):
                 return -1
             makeMove(game['board'], 'O', move)
             if isWinner(game['board'], 'O'):
-                print('O won the game')
                 game['isPlaying'] = False
                 return 2
             else:
                 if isBoardFull(game['board']):
-                    print('Tie')
                     return 3
                 else:
                     game['turn'] = 'X'
                     game['turnuid'] = game['X']
                     return 0
-    else:
-        print("Tried to move in a game that finished")
 
 
-def serve_forever(host, port):
+def startServer(host, port):
     lstsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lstsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     lstsock.setblocking(0)
@@ -101,7 +95,6 @@ def serve_forever(host, port):
     lstsock.listen(BACKLOG)
     print('Serving up some Tic-Tac-Toe on ' + str(host) + ':' + str(port) + '...')
     rlist, wlist, elist = [lstsock], [], []
-
     while True:
         readables, writables, exceptions = select.select(rlist, wlist, elist)
         for sock in readables:
@@ -122,14 +115,17 @@ def serve_forever(host, port):
                     rlist.remove(sock)
                 else:
                     info = bytes.split('#')
-                    print(info)
                     if info[0] == '100':
                         name = info[1]
                         if name not in users:
                             users[name] = 'availible'
                             sock.sendall(('101#' + name).encode())
                         else:
-                            sock.sendall(('401#').encode())
+                            sock.sendall(('401#Username already taken').encode())
+                    elif info[0] == '110':
+                        del users[info[1]]
+                        sock.close()
+                        rlist.remove(sock)
                     elif info[0] == '130':
                         if info[2] == '1':
                             responce = ''
@@ -148,27 +144,22 @@ def serve_forever(host, port):
                             if game is not None:
                                 if (game['O'] == info[1] or game['X'] == info[1]) and game['isPlaying']:
                                     found = True
-                                    print('213#' + str(i) + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O'])
                                     sock.sendall(('213#' + str(i) + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O']).encode())
                                 elif (game['O'] == info[1] or game['X'] == info[1]) and game['isPlaying'] is False:
                                     found = True
-                                    print('game over')
                                     if isWinner(game['board'], 'X'):
-                                        sock.sendall(('214#' + info[1] + '#' + 'X Won!').encode())
-                                    elif isWinner(game['board'], 'O'):
                                         sock.sendall(('214#' + info[1] + '#' + 'O Won!').encode())
+                                    elif isWinner(game['board'], 'O'):
+                                        sock.sendall(('214#' + info[1] + '#' + 'X Won!').encode())
                                     else:
                                         sock.sendall(('214#' + info[1] + '#' + 'Tie!').encode())
                                     games[i] = None
                         if not found:
-                            print('game not foumnd')
                             sock.sendall(('198#').encode())
                     elif info[0] == '200':
                         if info[2] not in users:
-                            print('player doesnt exist')
                             sock.sendall(('408#' + info[1] + '#Invited player does not exist').encode())
                         elif users[info[2]] == 'busy':
-                            print('player busy')
                             sock.sendall(('408#' + info[1] + '#Invited player busy').encode())
                         else:
                             gid = -1
@@ -182,10 +173,8 @@ def serve_forever(host, port):
                                 users[info[1]] = 'busy'
                                 users[info[2]] = 'busy'
                                 sock.sendall(('201#' + info[1] + '#' + str(gid)).encode())
-                                print('213#' + str(gid) + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O'])
                                 sock.sendall(('213#' + str(gid) + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O']).encode())
                             else:
-                                print('out of gid')
                                 sock.sendall(('408#' + info[1] + '#Out of game slots').encode())
                     elif info[0] == '204':
                         game = games[int(info[2])]
@@ -193,44 +182,33 @@ def serve_forever(host, port):
                         users[game['O']] = 'availible'
                         del users[info[1]]
                         game['isPlaying'] = False
-                        # game = None
                         sock.sendall(("205#" + info[1] + '#' + info[2]).encode())
                         sock.close()
                         rlist.remove(sock)
                     elif info[0] == '210':
                         ret = move(int(info[2]), info[3])
                         if ret == 0 or ret == -1:
-                            print('0 or -1')
                             sock.sendall(("211#" + info[1] + '#' + info[3]).encode())
                         elif ret == 1:
-                            print('214#' + info[2] + '#' + 'X Won!')
                             game = games[int(info[2])]
                             users[game['X']] = 'availible'
                             users[game['O']] = 'availible'
                             game['isPlaying'] = False
-                            # game['X'] = None
-                            # game = None
-                            sock.sendall(('214#' + info[2] + '#' + 'X Won!').encode())
-                        elif ret == 2:
-                            print('2')
-                            game = games[int(info[2])]
-                            users[game['X']] = 'availible'
-                            users[game['O']] = 'availible'
-                            game['isPlaying'] = False
-                            # game['O'] = None
-                            # game = None
                             sock.sendall(('214#' + info[2] + '#' + 'O Won!').encode())
-                        elif ret == 3:
-                            print('3')
+                        elif ret == 2:
                             game = games[int(info[2])]
                             users[game['X']] = 'availible'
                             users[game['O']] = 'availible'
                             game['isPlaying'] = False
-                            # game = None
+                            sock.sendall(('214#' + info[2] + '#' + 'X Won!').encode())
+                        elif ret == 3:
+                            game = games[int(info[2])]
+                            users[game['X']] = 'availible'
+                            users[game['O']] = 'availible'
+                            game['isPlaying'] = False
                             sock.sendall(('214#' + info[2] + '#' + 'Tie!').encode())
                     elif info[0] == '212':
                         game = games[int(info[2])]
-                        print('213#' + info[2] + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O'])
                         sock.sendall(('213#' + info[2] + '#' + ''.join(game['board']) + '#' + game['turnuid'] + '#' + game['X'] + '#' + game['O']).encode())
 
                     else:
@@ -239,15 +217,10 @@ def serve_forever(host, port):
 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option(
-        '-i', '--host', dest='host', default='localhost',
-        help='Hostname or IP address. Default is localhost'
-    )
-    parser.add_option(
-        '-p', '--port', dest='port', type='int', default=69,
-        help='Port. Default is 69')
+    parser.add_option('-i', dest='host', default='localhost', help='Hostname or IP address. Default is localhost')
+    parser.add_option('-p', dest='port', type='int', default=9206, help='Port. Default is 9206')
     options, args = parser.parse_args()
-    serve_forever(options.host, options.port)
+    startServer(options.host, options.port)
 
 
 if __name__ == '__main__':
